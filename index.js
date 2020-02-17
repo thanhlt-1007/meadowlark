bd7cef80e37666023603008ea88f267d6740ccd3
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2020-02-17 14:01:13 +0700

    add_root_and_about

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.listen(app.get('port'), function() {
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl + C to terminate.');
 });
 
+// have to set route before 400 and 500 page
+
+// get /
+app.get('/', function(req, res) {
+  res.type('text/plain');
+  res.status(200);
+  res.send('Home page');
+});
+
+// get /about
+app.get('/about', function(req, res) {
+  res.type('text/plain');
+  res.status(200);
+  res.send('About page');
+});
+
 // custom 400 page
 app.use(function(req, res) {
   res.type('text/plain');
